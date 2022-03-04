19995c4ef2e4346f3f69a1dadf9891a990ddf76a
Jonatan Madera <jonamade2@gmail.com>
2022-03-04 17:17:05 +0100

    first commit infra

---
+++ vpc.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  default     = "eu-west-1"
+  description = "AWS region"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.2.0"
+
+  name                 = "ta-vpc-infra"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24"]
+  public_subnets       = ["10.0.4.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+
+}
