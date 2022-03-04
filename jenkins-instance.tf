5775895a0c5d4d7adb54e925e6171a2225ed3300
Jonatan Madera <jonamade2@gmail.com>
2022-03-04 20:26:16 +0100

    trying to fix issues with network/vpc

--- jenkins-instance.tf
+++ jenkins-instance.tf
@@ -3,18 +3,15 @@ resource "aws_instance" "ec2" {
   ami         = "ami-080af029940804103"
   instance_type = "t2.micro"
   key_name = aws_key_pair.deployer.key_name
+#   vpc_security_group_ids = [aws_security_group.jenkins-sg.id]
 
   tags = {
-    name = "Jenkins"
+    Name = "Jenkins"
   }
 
   # the VPC subnet
   # subnets = module.vpc.public_subnets[0].id
 
-  # the security group
-  vpc_security_group_ids = [aws_security_group.jenkins-sg.id]
-
-  # the public SSH key
 
   network_interface {
     network_interface_id = aws_network_interface.net.id
@@ -25,7 +22,6 @@ resource "aws_instance" "ec2" {
 
 resource "aws_network_interface" "net" {
   subnet_id = aws_subnet.main-public-1.id
-
   tags = {
     Name = "primary_network_interface"
   }
